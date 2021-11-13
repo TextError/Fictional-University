92d98850386c322cb2c7f3a736812a242162c67c
TextError <savinraul1983@yahoo.com>
2021-11-13 06:22:30 +0000

    Automation Workflow

--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
   function load_files() {
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
     wp_enqueue_style('Roboto-Font', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('Font-Awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('main-university-css', get_stylesheet_uri());
+
+    if(strstr($_SERVER['SERVER_NAME'], 'fictionaluniversity.local')) {
+      wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    } else {
+      wp_enqueue_script('vendor.js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+      wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
+      wp_enqueue_style('main-university-css', get_stylesheet_uri('/budled-assets/styles.css'));
+    };
+
+    // wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    // wp_enqueue_style('main-university-css', get_stylesheet_uri());
   };
 
   // Adds title tag to page.
