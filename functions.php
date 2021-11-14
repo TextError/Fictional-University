2539c40173edb49405b45d977b3e8b38fb327117
TextError <savinraul1983@yahoo.com>
2021-11-14 06:28:06 +0000

    Added new Program page

--- functions.php
+++ functions.php
@@ -24,6 +24,12 @@
   };
   function adjust_queries($query) {
     $today = date('Ymd');
+    if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+      $query->set("orderby", "title");
+      $query->set("order", "ASC");
+      $query->set("posts_per_page", -1);
+    };
+
     if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
       $query->set("meta_key", "event_date");
       $query->set("orderby", "meta_value_num");
